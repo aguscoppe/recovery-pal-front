41fbc90a654385e1fd2580b8b9d000fc57336810
daterro <diego.a.terron@exxonmobil.com>
2022-10-15 19:18:33 -0300

    Updated a few files
    done

--- src/Pages/HomePacienteRoutine.js
+++ src/Pages/HomePacienteRoutine.js
@@ -5,17 +5,16 @@ import HomeIcon from '@mui/icons-material/Home';
 import Header from '../Components/Header';
 import NavBar from '../Components/NavBar';
 import CardExercise from '../Components/CardExercise';
-import { ejercicios } from '../ejercicios';
-import Rutine from '../Components/Rutine';
-import {rutinas } from '../rutinas';
 import CardRoutine from '../Components/CardRoutine';
+import {routines} from '../data';
+import Routine from '../Components/Routine'
 
 function showRoutines(routine) {
   console.log(routine);
   console.log(routine.exercises.lenght);
   return (
     <Grid item xs={11} md={12} xl={11} lg={11}>
-      <Rutine id={routine.idRoutine} idPaciente = {routine.pacient} name={routine.name} imgSrc= {routine.imgSrc} frecuencia={routine.schedule.weeks} duracion={routine.schedule.times} cantidad={routine.exercises.lenght} isComplete={false}/>
+      <Routine id={routine.idRoutine} idPaciente = {routine.pacient} name={routine.name} imgSrc= {routine.imgSrc} frecuencia={routine.schedule.weeks} duracion={routine.schedule.times} cantidad={routine.exercises.lenght} isComplete={false}/>
     </Grid>
   );
 }
@@ -32,7 +31,7 @@ const HomePacienteRoutine = () => {
         sx={{ padding: '10vh 0' }}
         spacing={2}
       >
-        {rutinas.map(showRoutines)}
+        {routines.map(showRoutines)}
       </Grid>
       <NavBar />
     </>
