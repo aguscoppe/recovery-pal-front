26efc1bce34449e7f3e4bfd7d2607b0a99d6b28d
JuanIgnacioUrru <94595304+JuanIgnacioUrru@users.noreply.github.com>
2022-10-27 00:30:17 -0300

    finalizacion barra progeso

--- src/Components/Routine.js
+++ src/Components/Routine.js
@@ -15,7 +15,7 @@ import LinearProgress, {
 } from "@mui/material/LinearProgress";
 
 
-
+//Aca esta el stylizado de la barra de progreso
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 15,
   borderRadius: 5,
@@ -28,15 +28,25 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   },
 }));
 
-const Routine = ({ routine }) => {
-  const { _id, name, exercises, isComplete } = routine;
+
+
+
+const Routine = ({ routine }) => {   //se recibe ya una rutina
+  //para mejor entendimiento del codigo se guardan en variables los valores de esta rutina
+  const _id = routine._id;
+  const name = routine.name;
+  const sessions = routine.feedbacks;
+  const exercises = routine.exercises;
   const duration = routine.schedule.weeks;
   const frequency = routine.schedule.times;
+  const feedbacksDone = routine.feedbacksDone;
+  const firstExercise = routine.exercises[0].exercise;
+
+
+  //todo lo siguiente esta comentado porque es otra forma de recuperar la rutina
+  /*
   const [exercise, setExercise] = useState(null);
-  console.log(exercises);
-
-  const [feedback, setFeedback] = useState(null);
-
+  const [feedback, setFeedback] = useState(null); // state con la cantidad de ejercicios realizados para poner en la barra de progreso
 
   useEffect(() => {
 
@@ -55,22 +65,23 @@ const Routine = ({ routine }) => {
     obtenerFeedback(_id);
 
     
-  }, [_id]);
+  }, [_id]);*/
 
-
+  /*
   const obtenerFeedback = async function(idRoutine){
     const feedback = await getLastFeedbackByRoutin(idRoutine);
     setFeedback(feedback.exercise.exercisesDone.length)
     //setFeedback(1)  --> esto es para ver que funcione en la practica es cambiado actualizando el feedback
     console.log(feedback.exercise);
   }
-
+*/
 
   return (
     <Link to={`/routine/${_id}`} style={{ textDecoration: "none" }}>
       <Card
         sx={
-          isComplete
+          //si esta completado es decir hay la misma cantidad de sessiones completas como de sessiones en total
+          (sessions.length===feedbacksDone)
             ? { border: "2px solid green", marginBottom: "12px" }
             : { marginBottom: "12px" }
         }
@@ -78,13 +89,14 @@ const Routine = ({ routine }) => {
         <Grid container justifyContent="center" alignItems="center">
           
           <Grid item xs={5} margin="10px">
-            {exercise === null ? (
+            {//si no hay ejercicios
+              exercises === null ? (
               <CircularProgress />
             ) : (
-              <ReactPlayer url={exercise.videoURL} width="100%" height="100%" />
+              <ReactPlayer url={firstExercise.videoURL} width="100%" height="100%" />
             )}
           </Grid>
-          <Grid item xs={isComplete ? 4 : 5} margin="10px">
+          <Grid item xs={(sessions===feedbacksDone) ? 4 : 5} margin="10px">
             <Grid
               sx={{
                 display: "flex",
@@ -108,7 +120,7 @@ const Routine = ({ routine }) => {
             
         
           </Grid>
-          {isComplete && (
+          {(sessions===feedbacksDone) && (
             <Grid item xs={1}>
               <IconButton>
                 <CheckCircleIcon sx={{ color: "green" }} />
@@ -116,10 +128,11 @@ const Routine = ({ routine }) => {
             </Grid>
           )}
           <Grid item xs={9} sx={{ paddingBottom: '2vh'}}>
-              <BorderLinearProgress variant="determinate" value={(feedback / exercises.length) *100}/>
+            {/*Para realizar el porcentaje: la cant de ejercicios por las sesiones completadas dividido la cant de ejercicios por el total de sesiones*/}
+              <BorderLinearProgress variant="determinate" value={((exercises.length*feedbacksDone) / (exercises.length*sessions.length)) *100}/>
           </Grid>
           <Grid item xs={1} sx={{ paddingBottom: '2vh'}}>
-          <Typography variant="body2">{(feedback / exercises.length) *100}%</Typography>
+          <Typography variant="body2">{((exercises.length*feedbacksDone) / (exercises.length*sessions.length))  *100}%</Typography>
           </Grid>
           
         </Grid>
@@ -129,3 +142,5 @@ const Routine = ({ routine }) => {
 };
 
 export default Routine;
+
+
