5695b6e0729399f00efc0b99f29136677d3c9ee0
PedroBlanco23 <pedriblanco@gmail.com>
2022-10-29 16:19:55 -0300

    se agrego para buscar por nombre al paciente

--- src/Components/SearchPatients.js
+++ src/Components/SearchPatients.js
@@ -1,8 +1,26 @@
 import { TextField, Box } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import Fab from '@mui/material/Fab';
+import { useContext, useState, useEffect } from 'react';
 
-function SearchPatients() {
+function SearchPatients(props) {
+
+  const [patientFilter, setPatientFilter] = useState(null)
+
+  function buscarPatients() {
+    props.setPatientsFiltered(
+      props.patients.filter((e) =>
+        `${e.name} ${e.lastName}`.toLowerCase().includes(patientFilter.toLowerCase())
+      )
+    );
+    
+  }
+
+  useEffect(() => {
+    props.setPatientsFiltered(props.patients)
+
+  }, [])
+
   return (
     <Box display='flex' flexDirection='row'>
       <TextField
@@ -11,10 +29,10 @@ function SearchPatients() {
         style={{ width: 300 }}
         label='Ingrese mail del paciente'
         variant='outlined'
-        onChange={null}
+        onChange={(e) => setPatientFilter(e.target.value)}
         sx={null}
       />
-      <Fab color='primary' aria-label='search' sx={{ ml: 2 }}>
+      <Fab color='primary' aria-label='search' sx={{ ml: 2 }} onClick={buscarPatients}>
         <SearchIcon />
       </Fab>
     </Box>
