a2a116d45eef752bbb10a4b00eedbe06a2b7f755
JuanIgnacioUrru <94595304+JuanIgnacioUrru@users.noreply.github.com>
2022-10-31 22:33:04 -0300

    Cambio en la cards de ejercicios

--- src/Components/Exercise.js
+++ src/Components/Exercise.js
@@ -1,43 +1,66 @@
-import { Grid, Card, Typography, IconButton } from '@mui/material';
-import { Link } from 'react-router-dom';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import DeleteIcon from '@mui/icons-material/Delete';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ReactPlayer from 'react-player';
+import { Grid, Card, Typography, IconButton } from "@mui/material";
+import { Link } from "react-router-dom";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ReactPlayer from "react-player";
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 
 const Exercise = ({ exercise }) => {
-  const { _id, videoURL, videoTitle, instructions, action } =
-    exercise.exercise;
-  const {isComplete } = exercise //ya que is complete esta sobre el ejercicio con peso y set
+  const { _id, videoURL, videoTitle, instructions, action } = exercise.exercise;
+  const {sets,weight,repetitions}=exercise
+  const { isComplete } = exercise; //ya que is complete esta sobre el ejercicio con peso y set
+  
 
-  console.log(exercise)
+  console.log(exercise);
   return (
-    <Link to={`exercise/${_id}`} style={{ textDecoration: 'none' }}>
+    <Link to={`exercise/${_id}`} style={{ textDecoration: "none" }}>
       <Card
         sx={
           isComplete
-            ? { border: '2px solid green', marginBottom: '12px' }
-            : { marginBottom: '12px' }
+            ? { border: "2px solid green", marginBottom: "12px" }
+            : { marginBottom: "12px" }
         }
       >
-        <Grid container alignItems='center'>
-          <Grid item xs={6} margin='10px'>
-            <ReactPlayer  url={videoURL} width='100%' height='100%' />
+        <Grid container alignItems="center" justifyContent="center">
+          <Grid item xs={12} margin="10px">
+          <Typography align="center" variant="h6" sx={{ fontWeight: "500" }}>
+                {videoTitle}
+              </Typography>
           </Grid>
-          <Grid item xs={4} margin='10px'>
-            <Typography variant='h6' sx={{ fontWeight: '500' }}>
-              {videoTitle}
-            </Typography>
-            <Typography sx={{ fontSize: '12px' }}>{instructions}</Typography>
+          <Grid item xs={6} margin="10px">
+            <ReactPlayer url={videoURL} width="100%" height="100%" />
+          </Grid>
+          <Grid item xs={4} margin="10px">
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+              }}
+            >
+              <Typography variant="body1" sx={{ fontWeight: "500" }}>
+                <AddTaskIcon /> {sets} Sets
+              </Typography>
+              <Typography variant="body1" sx={{ fontWeight: "500" }}>
+                <RepeatIcon /> {repetitions} Repeticiones
+              </Typography>
+              <Typography variant="body1" sx={{ fontWeight: "500" }}>
+                <FitnessCenterIcon /> {weight} Kg
+              </Typography>
+              
+            </Grid>
           </Grid>
           {action && (
             <Grid item xs={1}>
               <IconButton onClick={action.function}>
-                {action.type === 'add' && (
-                  <AddCircleIcon sx={{ color: 'lightblue' }} />
+                {action.type === "add" && (
+                  <AddCircleIcon sx={{ color: "lightblue" }} />
                 )}
-                {action.type === 'remove' && (
-                  <DeleteIcon sx={{ color: 'red' }} />
+                {action.type === "remove" && (
+                  <DeleteIcon sx={{ color: "red" }} />
                 )}
               </IconButton>
             </Grid>
@@ -45,7 +68,7 @@ const Exercise = ({ exercise }) => {
           {isComplete && (
             <Grid item xs={1}>
               <IconButton>
-                <CheckCircleIcon sx={{ color: 'green' }} />
+                <CheckCircleIcon sx={{ color: "green" }} />
               </IconButton>
             </Grid>
           )}
