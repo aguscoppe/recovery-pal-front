d0a9e00103958eb44d8ca1e9fef628bf2ea70a92
Federico <federicodiazgerstner@gmail.com>
2022-11-01 10:30:36 -0300

    Fix para creacion de ejercicios

--- src/Components/ModalExercise.js
+++ src/Components/ModalExercise.js
@@ -15,9 +15,10 @@ import CreateExercise from "../Pages/CreateExercise";
 import { exerciseCreation } from "../Controllers/ExerciseEntry.Controller";
 import { uploadVideo } from "../Controllers/VideoEntry.Controller";
 import { upload } from "@testing-library/user-event/dist/upload";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const ModalExercise = ({ open, handleClose }) => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useContext(UserContext);
 
   const [exerciseData, setExerciseData] = useState({
     name: "",
@@ -27,6 +28,7 @@ const ModalExercise = ({ open, handleClose }) => {
 
   const [selectedFile, setSelectedFile] = useState();
   const [encodedFile, setEncodedFile] = useState("");
+  const [showCircularProgress, setShowCircularProgress] = useState(false);
 
   const textFieldSpacing = {
     marginBottom: "20px",
@@ -83,9 +85,8 @@ const ModalExercise = ({ open, handleClose }) => {
   };
 
   const handleClick = async () => {
+    setShowCircularProgress(true);
     uploadVideoCloudinary();
-    createExercise();
-    handleClose();
   };
 
   const uploadVideoCloudinary = async () => {
@@ -97,8 +98,14 @@ const ModalExercise = ({ open, handleClose }) => {
       );
 
       if (res.rdo === 0) {
-        setExerciseData({ ...exerciseData, videoURL: res.videoData.url });
+        console.log("video data", res.videoData);
+        console.log("video URL", res.videoData.url);
+        const name = "videoURL";
+        const value = res.videoData.url;
+        setExerciseData({ ...exerciseData, [name]: value });
         console.log(res.message);
+        console.log("Creando el ejercicio...");
+        createExercise(value);
       } else {
         console.log(res.message);
       }
@@ -107,13 +114,14 @@ const ModalExercise = ({ open, handleClose }) => {
     }
   };
 
-  const createExercise = async () => {
+  const createExercise = async (value) => {
+    console.log("value", value);
     try {
       const res = await exerciseCreation({
         doctor: currentUser._id,
         instructions: exerciseData.description,
         videoTitle: exerciseData.name,
-        videoURL: exerciseData.videoURL,
+        videoURL: value,
       });
 
       if (res.rdo === 0) {
@@ -121,8 +129,12 @@ const ModalExercise = ({ open, handleClose }) => {
       } else {
         console.log("Fallo algo");
       }
+
+      setShowCircularProgress(false);
+      handleClose(res.rdo);
     } catch (e) {
       console.log(e);
+      handleClose(1);
     }
   };
 
@@ -160,26 +172,26 @@ const ModalExercise = ({ open, handleClose }) => {
               onChange={handleChangeFile}
             />
             <Grid item container justifyContent="center">
-              <Button
-                size="large"
-                variant="outlined"
-                onClick={handleClose}
-              >
+              <Button size="large" variant="outlined" onClick={handleClose}>
                 Cancelar
               </Button>
             </Grid>
             <Grid item container justifyContent="center">
-              <Button
-                disabled={
-                  exerciseData.name === "" || exerciseData.description === ""
-                }
-                size="large"
-                variant="contained"
-                onClick={handleClick}
-                sx= {{mt: 3}}
-              >
-                Finalizar
-              </Button>
+              {showCircularProgress ? (
+                <CircularProgress sx={{ marginTop: "10px" }} />
+              ) : (
+                <Button
+                  disabled={
+                    exerciseData.name === "" || exerciseData.description === ""
+                  }
+                  size="large"
+                  variant="contained"
+                  onClick={handleClick}
+                  sx={{ mt: 3 }}
+                >
+                  Finalizar
+                </Button>
+              )}
             </Grid>
           </Grid>
         </Grid>
