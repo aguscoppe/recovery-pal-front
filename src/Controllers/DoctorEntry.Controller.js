bd6d1c5462f646d9dadf8f58ded620ac6d275d45
PedroBlanco23 <pedriblanco@gmail.com>
2022-11-08 00:46:22 -0300

    entry de add patient to doctor, añadir comentario a rutina  y descripcion a ejercicio

--- src/Controllers/DoctorEntry.Controller.js
+++ src/Controllers/DoctorEntry.Controller.js
@@ -93,3 +93,47 @@ export const getDoctorById = async function(id){
 
     
 }
+
+export const addPatientToDoctor = async function(datos){
+    //url webservices
+    let url = urlWebServices.addPatientToDoctor;
+    //armo json con datos
+    const formData = new URLSearchParams();
+    formData.append('idDoctor', datos.idDoctor);
+    formData.append('idPatient', datos.idPatient);
+    //console.log("dato",formData);
+    //console.log("url",url);
+    try{
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+               // 'x-access-token': WebToken.webToken,
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formData,
+            
+        });
+        
+        let rdo = response.status;
+        console.log("response",response);
+        let data = await response.json();
+        console.log("jsonresponse",data);
+            switch(rdo){
+                case 202:
+                {
+                 
+                    return ({rdo:0,mensaje:"Ok"});//correcto
+                }
+                default:
+                {
+                    //otro error
+                    return ({rdo:1,mensaje:data.message});                
+                }
+            }
+    }
+    catch(error){
+        console.log("error",error);
+    };
+}
