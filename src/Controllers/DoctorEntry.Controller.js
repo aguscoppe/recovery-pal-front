0b562e8a05e8ca01955f6ca7b8dab3bdf9ed192c
Pedro Seva <pedrosevaseva@gmail.com>
2022-11-12 18:59:10 -0300

    add comment function

--- src/Controllers/DoctorEntry.Controller.js
+++ src/Controllers/DoctorEntry.Controller.js
@@ -137,3 +137,44 @@ export const addPatientToDoctor = async function(datos){
         console.log("error",error);
     };
 }
+
+export const getAllCommentsByDoctor = async function(idDoctor){
+    //url webservices
+    let url = urlWebServices.getAllCommentsByDoctor;
+    //armo json con datos
+    const formData = new URLSearchParams();
+    formData.append('idDoctor', idDoctor);
+    try{
+        let response = await fetch(url,{
+            method: 'POST', // or 'PUT'
+            mode: "cors",
+            headers:{
+                'Accept':'application/x-www-form-urlencoded',
+               // 'x-access-token': WebToken.webToken,
+                'Origin':'http://localhost:3000',
+                'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formData,
+            
+        });
+        
+        let rdo = response.status;
+        console.log("response",response);
+        let data = await response.json();
+        console.log("jsonresponse",data);
+            switch(rdo){
+                case 200:
+                {
+                 
+                    return ({rdo:0,res:data.comments});//correcto
+                }
+                default:
+                {
+                    //otro error
+                    return ({rdo:1,mensaje:data.message});                
+                }
+            }
+    }
+    catch(error){
+        console.log("error",error);
+    };
+}
