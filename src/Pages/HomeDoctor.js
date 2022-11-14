76d263daec90cdc8965d412477b3c8bee9e71309
Mercedes Rodriguez <mercedes-belen.rodriguez@endava.com>
2022-11-14 18:55:30 -0300

    change son homeDoctor

--- src/Pages/HomeDoctor.js
+++ src/Pages/HomeDoctor.js
@@ -1,32 +1,27 @@
-import { 
-  Grid,
-  Fab, 
-  TextField,
-  Typography,
-  Modal,
-  Box } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import Header from '../Components/Header';
-import NavBar from '../Components/NavBar';
-import PatientCard from '../Components/PatientCard';
+import { Grid, Fab, TextField, Typography, Modal, Box } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import Header from "../Components/Header";
+import NavBar from "../Components/NavBar";
+import PatientCard from "../Components/PatientCard";
 import AddIcon from "@mui/icons-material/Add";
-import { useContext, useState, useEffect } from 'react';
-import { getDoctorById } from '../Controllers/DoctorEntry.Controller';
-import { UserContext } from '../Contexts/UserContext';
-import CircularProgress from '@mui/material/CircularProgress';
-import SearchPatients from '../Components/SearchPatients';
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import { useContext, useState, useEffect } from "react";
+import { getDoctorById } from "../Controllers/DoctorEntry.Controller";
+import { UserContext } from "../Contexts/UserContext";
+import CircularProgress from "@mui/material/CircularProgress";
+import SearchPatients from "../Components/SearchPatients";
+import * as React from "react";
+import Button from "@mui/material/Button";
 import { theme } from "../theme";
-
+import ModalPatient from "../Components/ModalPatient";
+import ModalAlert from "../Components/ModalAlert";
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   pt: 2,
   px: 4,
@@ -46,7 +41,7 @@ const modalContainer = {
   backgroundColor: "#FFF",
   borderRadius: "20px",
 };
-const textFieldSpacing = { 
+const textFieldSpacing = {
   marginBottom: "20px",
 };
 const title = {
@@ -55,45 +50,50 @@ const title = {
   color: theme.palette.primary.main,
   marginBottom: "50px",
 };
-
 /* Esta pagina es el home que deberia ver el doctor con la lista de sus pacientes aun no conectada con el back */
 const HomeDoctor = () => {
   const currentUser = useContext(UserContext);
-
   const [doctor, setDoctor] = useState(null);
   const [patients, setPatients] = useState(null);
-  const [patientsFiltered, setPatientsFiltered] = useState(null)
-  const [patientData, setPatientData] = useState({
-    email: "",
+  const [patientsFiltered, setPatientsFiltered] = useState(null);
+  const [showModalPatient, setShowModalPatient] = useState(false);
+  const [modalAlert, setModalAlert] = useState({
+    open: false,
+    type: "",
+    title: "",
+    subtitle: "",
+    primaryBtnText: "",
+    primaryBtnPage: "",
+    setNotOpen: () => {},
   });
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => {
-    setOpen(true);
+  const handleAddPatient = () => {
+    setShowModalPatient(true);
   };
-  const handleClose = () => {
-    setOpen(false);
+  const handleCloseModal = (v) => {
+    setShowModalPatient(false);
+    if (v === 0) {
+      setModalAlert({
+        open: true,
+        type: "success",
+        title: "Bien hecho",
+        subtitle: "Se ha agregado el paciente correctamente a tu lista.",
+        primaryBtnText: "Continuar",
+        setNotOpen: () => {
+          setModalAlert((prev) => ({ ...prev, open: false }));
+        },
+      });
+    }
   };
-  const handleClick = async () => {
-    setDisableButton(true);
-  };
-  const [disableButton, setDisableButton] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPatientData({ ...patientData, [name]: value });
-  };
-
-
   useEffect(() => {
     const getDoctor = async function () {
       const respuestaDoctor = await getDoctorById(currentUser._id);
       console.log(
-        'Console log de respuesta de back ',
+        "Console log de respuesta de back ",
         JSON.stringify(respuestaDoctor)
       );
       if (respuestaDoctor.rdo === 1) {
-        alert('No existe el doctor');
-        window.location.href = '/';
+        alert("No existe el doctor");
+        window.location.href = "/";
       } else {
         setDoctor(respuestaDoctor.doctor);
         console.log(JSON.stringify(respuestaDoctor));
@@ -103,41 +103,60 @@ const HomeDoctor = () => {
     };
     getDoctor();
   }, [currentUser._id]);
-
   return (
     <>
-      <Header title='Pacientes' icon={<HomeIcon />} />
+      <Header title="Pacientes" icon={<HomeIcon />} />
+      {modalAlert.open && (
+        <ModalAlert
+          open={modalAlert.open}
+          type={modalAlert.type}
+          title={modalAlert.title}
+          subtitle={modalAlert.subtitle}
+          primaryBtnText={modalAlert.primaryBtnText}
+          primaryBtnPage={modalAlert.primaryBtnPage}
+          setNotOpen={modalAlert.setNotOpen}
+        />
+      )}
+      {showModalPatient && (
+        <ModalPatient open={showModalPatient} handleClose={handleCloseModal} />
+      )}
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        sx={{ paddingTop: "10vh" }}
+      >
         <Grid
-          container
-          direction='column'
-          justifyContent='center'
-          sx={{ paddingTop: '10vh' }}>
-          <Grid
-            item
-            xs={12}
-            md={12}
-            display='flex'
-            flexDirection='column'
-            alignItems='center'
-            justifyContent='center'>
-            <SearchPatients setPatientsFiltered= {setPatientsFiltered} patients={patients}/>
-          </Grid>
-          <Grid
-            item
-            xs={11}
-            md={6}
-            display='flex'
-            flexDirection='column'
-            alignItems='center'
-            justifyContent='center'>
-            {patientsFiltered === null ? (
-              <CircularProgress />
-            ) : (
-            patientsFiltered.map((e) => <PatientCard patient={e} />)
-            )}
-          </Grid>
+          item
+          xs={12}
+          md={12}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <SearchPatients
+            setPatientsFiltered={setPatientsFiltered}
+            patients={patients}
+          />
         </Grid>
-      <Fab 
+        <Grid
+          item
+          xs={11}
+          md={6}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {patientsFiltered === null ? (
+            <CircularProgress />
+          ) : (
+            patientsFiltered.map((e) => <PatientCard patient={e} />)
+          )}
+        </Grid>
+      </Grid>
+      <Fab
         color="primary"
         aria-label="add"
         sx={{
@@ -145,52 +164,12 @@ const HomeDoctor = () => {
           bottom: 80,
           right: 25,
         }}
-        onClick={handleOpen}>
-        <AddIcon/>
+        onClick={handleAddPatient}
+      >
+        <AddIcon />
       </Fab>
-       <Modal open={open} onClose={handleClose}>
-            <Box sx={modalContainer}>
-                <Grid  item xs={12} sm={6} m={4}>
-                    <Typography variant="h5" sx={title}>
-                    Añadir paciente
-                    </Typography>
-                    <TextField
-                        name="email"
-                        value={patientData.email}
-                        fullWidth
-                        label="Email"
-                        variant="outlined"
-                        onChange={handleChange}
-                        sx={textFieldSpacing}
-                    />
-                    <Grid item container justifyContent="center">
-                        <Button
-                            size="large"
-                            variant="outlined"
-                            onClick={handleClose}
-                            disabled={disableButton}>
-                            Cancelar
-                        </Button>
-                    </Grid>
-                    <Grid item container justifyContent="center">
-                        <Button
-                            disabled={
-                                patientData.email === "" 
-                            }
-                            size="large"
-                            variant="contained"
-                            onClick={handleClick}
-                            sx={{ mt: 3 }}
-                            >
-                            Añadir
-                        </Button>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Modal> 
       <NavBar />
     </>
   );
 };
-
 export default HomeDoctor;
