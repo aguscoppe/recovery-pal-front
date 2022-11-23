cc4a5351c9b340451f5b35b1faafdc5de4839d4e
Federico <fdiaz@amherst.com>
2022-11-23 00:03:22 -0300

    Small fixes

--- src/Pages/HomeDoctor.js
+++ src/Pages/HomeDoctor.js
@@ -1,17 +1,17 @@
-import { Grid, Fab, Modal} from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import Header from "../Components/Header";
-import NavBar from "../Components/NavBar";
-import PatientCard from "../Components/PatientCard";
-import AddIcon from "@mui/icons-material/Add";
-import { useContext, useState, useEffect } from "react";
-import { getDoctorById } from "../Controllers/DoctorEntry.Controller";
-import { UserContext } from "../Contexts/UserContext";
-import CircularProgress from "@mui/material/CircularProgress";
-import SearchPatients from "../Components/SearchPatients";
-import * as React from "react";
-import ModalPatient from "../Components/ModalPatient";
-import ModalAlert from "../Components/ModalAlert";
+import { Grid, Fab, Modal } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import Header from '../Components/Header';
+import NavBar from '../Components/NavBar';
+import PatientCard from '../Components/PatientCard';
+import AddIcon from '@mui/icons-material/Add';
+import { useContext, useState, useEffect } from 'react';
+import { getDoctorById } from '../Controllers/DoctorEntry.Controller';
+import { UserContext } from '../Contexts/UserContext';
+import CircularProgress from '@mui/material/CircularProgress';
+import SearchPatients from '../Components/SearchPatients';
+import * as React from 'react';
+import ModalPatient from '../Components/ModalPatient';
+import ModalAlert from '../Components/ModalAlert';
 
 /* Esta pagina es el home que deberia ver el doctor con la lista de sus pacientes aun no conectada con el back */
 const HomeDoctor = () => {
@@ -22,11 +22,11 @@ const HomeDoctor = () => {
   const [showModalPatient, setShowModalPatient] = useState(false);
   const [modalAlert, setModalAlert] = useState({
     open: false,
-    type: "",
-    title: "",
-    subtitle: "",
-    primaryBtnText: "",
-    primaryBtnPage: "",
+    type: '',
+    title: '',
+    subtitle: '',
+    primaryBtnText: '',
+    primaryBtnPage: '',
     setNotOpen: () => {},
   });
   const handleAddPatient = () => {
@@ -37,10 +37,10 @@ const HomeDoctor = () => {
     if (v === 0) {
       setModalAlert({
         open: true,
-        type: "success",
-        title: "Bien hecho",
-        subtitle: "Se ha agregado el paciente correctamente a tu lista.",
-        primaryBtnText: "Continuar",
+        type: 'success',
+        title: 'Bien hecho',
+        subtitle: 'Se ha agregado el paciente correctamente a tu lista.',
+        primaryBtnText: 'Continuar',
         setNotOpen: () => {
           setModalAlert((prev) => ({ ...prev, open: false }));
         },
@@ -51,12 +51,12 @@ const HomeDoctor = () => {
     const getDoctor = async function () {
       const respuestaDoctor = await getDoctorById(currentUser._id);
       console.log(
-        "Console log de respuesta de back ",
+        'Console log de respuesta de back ',
         JSON.stringify(respuestaDoctor)
       );
       if (respuestaDoctor.rdo === 1) {
-        alert("No existe el doctor");
-        window.location.href = "/";
+        alert('No existe el doctor');
+        window.location.href = '/';
       } else {
         setDoctor(respuestaDoctor.doctor);
         console.log(JSON.stringify(respuestaDoctor));
@@ -68,7 +68,7 @@ const HomeDoctor = () => {
   }, [currentUser._id]);
   return (
     <>
-      <Header title="Pacientes" icon={<HomeIcon />} />
+      <Header title='Pacientes' icon={<HomeIcon />} />
       {modalAlert.open && (
         <ModalAlert
           open={modalAlert.open}
@@ -85,18 +85,18 @@ const HomeDoctor = () => {
       )}
       <Grid
         container
-        direction="column"
-        justifyContent="center"
-        sx={{ paddingTop: "10vh" }}
+        direction='column'
+        justifyContent='center'
+        sx={{ paddingTop: '10vh' }}
       >
         <Grid
           item
           xs={12}
           md={12}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
+          display='flex'
+          flexDirection='column'
+          alignItems='center'
+          justifyContent='center'
         >
           <SearchPatients
             setPatientsFiltered={setPatientsFiltered}
@@ -107,23 +107,25 @@ const HomeDoctor = () => {
           item
           xs={11}
           md={6}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
+          display='flex'
+          flexDirection='column'
+          alignItems='center'
+          justifyContent='center'
         >
           {patientsFiltered === null ? (
             <CircularProgress />
           ) : (
-            patientsFiltered.map((e) => <PatientCard patient={e} />)
+            patientsFiltered.map((e, index) => (
+              <PatientCard patient={e} key={index} />
+            ))
           )}
         </Grid>
       </Grid>
       <Fab
-        color="primary"
-        aria-label="add"
+        color='primary'
+        aria-label='add'
         sx={{
-          position: "fixed",
+          position: 'fixed',
           bottom: 80,
           right: 25,
         }}
